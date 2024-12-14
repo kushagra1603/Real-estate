1a88e922a527124458de4d75d175f74882f302e5
kushagra1603 <kushagraagarwal73@gmail.com>
2024-12-14 13:54:45 +0530

    production ready app

--- api/app.js
+++ api/app.js
@@ -8,11 +8,12 @@ import userRoute from "./routes/user.route.js";
 import chatRoute from "./routes/chat.route.js";
 import messageRoute from "./routes/message.route.js";
 import dotenv from "dotenv";
-
+import path from "path";
 
 dotenv.config();
 
 const app = express();
+const _dirname = path.resolve();
 
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
@@ -24,6 +25,11 @@ app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
 app.use("/api/chats", chatRoute);
 app.use("/api/messages", messageRoute);
+
+app.use(express.static(path.join(_dirname, "/client/dist")));
+app.get("*", (_, res) => {
+  res.sendFile(path.resolve(_dirname, "client", "dist", "index.html"));
+});
 
 app.listen(8800, () => {
   console.log("Server is running!");

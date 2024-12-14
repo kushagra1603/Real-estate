46f50ff7971e960d43fdbf5b006284b0fcc89632
kushagra1603 <kushagraagarwal73@gmail.com>
2024-12-14 21:21:00 +0530

    map

--- client/src/components/pin/Pin.jsx
+++ client/src/components/pin/Pin.jsx
@@ -1,17 +1,28 @@
 import { Marker, Popup } from "react-leaflet";
 import "./pin.scss";
 import { Link } from "react-router-dom";
+import L from "leaflet";
+
+// Create a default Leaflet icon
+const defaultIcon = L.icon({
+  iconUrl: "https://unpkg.com/leaflet@1.7.1/dist/images/marker-icon.png",
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [0, -41],
+});
+
+L.Marker.prototype.options.icon = defaultIcon; // Assign default icon to Marker
 
 function Pin({ item }) {
   return (
     <Marker position={[item.latitude, item.longitude]}>
       <Popup>
         <div className="popupContainer">
-          <img src={item.images[0]} alt="" />
+          <img src={item.images[0]} alt="" className="property-image" />
           <div className="textContainer">
             <Link to={`/${item.id}`}>{item.title}</Link>
             <span>{item.bedroom} bedroom</span>
-            <b>₹ {item.price}</b>
+            <b>₹ {item.price.toLocaleString()}</b>
           </div>
         </div>
       </Popup>
